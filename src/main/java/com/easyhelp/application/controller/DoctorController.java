f3d0b2c659a78f10e095719c9b94b9084d5c4776
Unknown <stefan.georgescu.970@gmail.com>
2018-09-11 20:03:38 +0300

    updated controller

--- src/main/java/com/easyhelp/application/controller/DoctorController.java
+++ src/main/java/com/easyhelp/application/controller/DoctorController.java
@@ -1,16 +1,22 @@
 package com.easyhelp.application.controller;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.easyhelp.application.model.Doctor;
+import com.easyhelp.application.service.DoctorServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class ControllerC {
+public class DoctorController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private DoctorServiceInterface doctorServiceInterface;
 
     @CrossOrigin
     @PostMapping("/api/hello-world")
@@ -18,4 +24,12 @@ public class ControllerC {
     public Doctor sayHello(@RequestBody(required = false) String body, @RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
         return new Doctor("Stefan", "Georgescu");
     }
+
+    @CrossOrigin
+    @GetMapping("/api/doctors")
+    @ResponseBody
+    public List<Doctor> getAllDoctors() {
+        return doctorServiceInterface.getAllDoctors();
+    }
+
 }
